8d9afaec69200d0cc22cddcf85366a9c15bbb329
ZakharovaAlina <113230084+ZakharovaAlina@users.noreply.github.com>
2022-10-20 21:15:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def _min(file_name):
+    data = sorted([int(i) for i in open(file_name, 'r').read().split(' ')])
+    return data[0]
+def _max(file_name):
+    data = sorted([int(i) for i in open(file_name, 'r').read().split(' ')])
+    return data[-1]
+def _sum(file_name):
+    data = sorted([int(i) for i in open(file_name, 'r').read().split(' ')])
+    return sum(data)
+def _mult(file_name):
+    data = sorted([int(i) for i in open(file_name, 'r').read().split(' ')])
+    a = 1
+    for i in data:
+        a*=i
+    return a
